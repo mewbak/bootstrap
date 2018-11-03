f34c22b1b60abe9697323a13b7110cdad885491b
Andy Williams <andy@andywilliams.me>
2018-11-03 21:44:15 +0000

    Check debian version, we need buster

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,6 +19,12 @@ INSTALL_COMMAND=""
 if [[ -e "/etc/arch-release" ]] || [[ -e "/etc/manjaro-release" ]]; then
   INSTALL_COMMAND="sudo pacman --noconfirm -S"
 elif [[ -e "/etc/debian_version" ]]; then
+  VERSION=`cat /etc/debian_version`
+  if [[ ${VER%.*} -lt 10 ]]; then
+    echo "[ERROR] Bootstrap requires debian 10 (Buster) or later"
+    exit 1
+  fi
+
   INSTALL_COMMAND="sudo apt-get install -q -y"
   DEP_LIST="git golang libefl-all-dev libssl-dev"
   DEP_FILE_LIST="git go ecore_evas_convert notestforssl"
